1f4ee7f67d4bad3a905544a77a47d60159c93130
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2023-06-09 14:24:40 +0600

    Create SqureRoot.java

---
+++ LeetCode/SqureRoot.java
@@ -0,0 +1,8 @@
+public class SqureRoot {
+    public static void main(String[] args) {
+        int x =8;
+     int ans= (int) Math.sqrt(x);
+        System.out.println(ans);
+    }
+}
+//Leetcode easy
